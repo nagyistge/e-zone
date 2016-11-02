a6ffcc3e5fba6a53660fca8bfffcbd8a1412444c
yz-wanjiaqing <wanjiaqing@easy-all.net>
2016-11-02 18:05:36 +0800

    test5

--- Application/Lib/Action/Wechat/IndexAction.class.php
+++ Application/Lib/Action/Wechat/IndexAction.class.php
@@ -63,11 +63,10 @@ class IndexAction extends Action {
 		$callback = [];
 		foreach ($result as $key => $value) {
 			$callback['lists'][$key]['pic'] = "http://".$_SERVER['HTTP_HOST']. "/Public/Uploads/img/" .$value['img'];
-			// $callback['lists'][$key]['link'] = $value['url'];
-			// if($value['type'] == 3 || $value['type'] == 4) {
-				
-			// }
-			$callback['lists'][$key]['link'] = "http://".$_SERVER['HTTP_HOST']. "/Public/Uploads/erweima/" .$value['erweima'];
+			$callback['lists'][$key]['link'] = $value['url'];
+			if($value['type'] == 3 || $value['type'] == 4) {
+				$callback['lists'][$key]['link'] = "http://".$_SERVER['HTTP_HOST']. "/Public/Uploads/erweima/" .$value['erweima'];
+			}
 		}
 		echo json_encode($callback);
 	}
