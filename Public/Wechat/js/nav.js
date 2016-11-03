3a98041e4edf2ccec7b5d2386707dece1347b862
yz-mengxiangsong <mengxiangsong@easy-all.net>
2016-11-03 14:02:12 +0800

    修改导航

--- Public/Wechat/js/nav.js
+++ Public/Wechat/js/nav.js
@@ -4,6 +4,9 @@ $(window).on('load', function() {
 	$('.preloader').delay(320).fadeOut('slow', function() {
 	});
 });
+$(".menu").find('li').eq(1).click(function(event) {
+	window.location.href = "{:U('Wechat/Index/index')}";
+});
 if ('ontouchstart' in window) {
 		var click = 'touchstart';
 	} else {
@@ -16,7 +19,7 @@ if ('ontouchstart' in window) {
 			closeMenu();
 		}
 	});
-	$('div.menu ul li a').on(click, function (e) {
+	$('div.menu ul li').on(click, function (e) {
 		e.preventDefault();
 		closeMenu();
 	});
