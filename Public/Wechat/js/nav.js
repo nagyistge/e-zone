08646e983bbb1a449ded066492fd3b74daf115a7
yz-mengxiangsong <mengxiangsong@easy-all.net>
2016-11-03 13:52:36 +0800

    nav.js

--- Public/Wechat/js/nav.js
+++ Public/Wechat/js/nav.js
@@ -16,7 +16,7 @@ if ('ontouchstart' in window) {
 			closeMenu();
 		}
 	});
-	$('div.menu ul li a').on(click, function (e) {
+	$('div.menu ul li').on(click, function (e) {
 		e.preventDefault();
 		closeMenu();
 	});
