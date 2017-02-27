96a5975528f477824792ba1b325da107526cce27
wanjiaqing@easy-all.net <wanjiaqing123>
2017-02-27 17:18:04 +0800

    upd

--- Public/Conf/config.php
+++ Public/Conf/config.php
@@ -4,7 +4,7 @@
 	
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
-	define('DB_PWD', 'pjmishi@#$123');
+	define('DB_PWD', 'pjmishi@#$123');  
 	define('DB_NAME', 'e_zone');
 	define('DB_PREFIX', 'yz_');
 	
