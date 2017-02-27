e31cb2555e9e04d97ee9d525b178542ad6e5e16b
wanjiaqing@easy-all.net <wanjiaqing123>
2017-02-27 17:27:30 +0800

    upd

--- Public/Conf/config.php
+++ Public/Conf/config.php
@@ -5,7 +5,7 @@
 	define('DB_HOST', 'localhost');
 	define('DB_USER', 'root');
 	define('DB_PWD', 'pjmishi@#$123');  
-	define('DB_NAME', 'e_zone');
+	define('DB_NAME', 'e-zone');
 	define('DB_PREFIX', 'yz_');
 	
 	$conn = @mysql_connect(DB_HOST, DB_USER, DB_PWD) or die('数据库链接失败：'.mysql_error());
