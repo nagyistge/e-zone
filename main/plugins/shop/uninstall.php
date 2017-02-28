07f8719554530378af4f02eb808bb9abbfa81b1e
wanjiaqing@easy-all.net <wanjiaqing@easy-all.net>
2017-02-28 13:32:05 +0800

    upd

--- main/plugins/shop/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('IN_FINECMS')) exit('No permission resources');
-
-return array(
-    "DROP TABLE IF EXISTS `{prefix}shop_address`;",
-    "DROP TABLE IF EXISTS `{prefix}shop_order`;",
-    "DROP TABLE IF EXISTS `{prefix}shop_order`;"
-);
